baa4d4d5f8a048c288b4a65f7c359c7c9621da4b
MarcoGMC-disgn <greenmobilitycuracao@gmail.com>
2025-07-10 20:15:42 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookingFlow from './components/BookingFlow';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-black p-4">
+      <h1 className="text-3xl font-bold mb-4">Green Mobility Curaçao</h1>
+      <BookingFlow />
+    </div>
+  );
+}
+
+export default App;
